cd76d4757cf0554fa54b7c1083a14f9214ab99ab
soohan <soohan0808@famoz.co.kr>
2023-01-08 14:21:21 +0900

    ✨ matchesTab 컴포넌트 제작

--- src/pages/summoners/Matchs/Tab/index.tsx
+++ src/pages/summoners/Matchs/Tab/index.tsx
@@ -1,12 +1,44 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import { gameType } from "../types";
 
-const TabComponent = () => {
+interface Props {
+  className?: string;
+  activeTab: gameType;
+  setActiveTab: React.Dispatch<React.SetStateAction<gameType>>;
+}
+
+const TabComponent = ({ activeTab, setActiveTab, className }: Props) => {
+  const active1 = "all";
+  const active2 = "solo";
+  const active3 = "free";
+
+  const handleSelectedTab = (selectedTab: gameType) => {
+    setActiveTab(selectedTab);
+  };
+
   return (
-    <Container>
-      <Tab isActive>전체</Tab>
-      <Tab isActive>솔로게임</Tab>
-      <Tab isActive>자유랭크</Tab>
+    <Container className={className}>
+      <TabContainer>
+        <Tab
+          onClick={() => handleSelectedTab(active1)}
+          isActive={activeTab === active1}
+        >
+          전체
+        </Tab>
+        <Tab
+          onClick={() => handleSelectedTab(active2)}
+          isActive={activeTab === active2}
+        >
+          솔로게임
+        </Tab>
+        <Tab
+          onClick={() => handleSelectedTab(active3)}
+          isActive={activeTab === active3}
+        >
+          자유랭크
+        </Tab>
+      </TabContainer>
     </Container>
   );
 };
@@ -14,17 +46,45 @@ const TabComponent = () => {
 export default TabComponent;
 
 const Container = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: flex-start;
   background: ${({ theme }) => theme.colors.offWhite};
   border: 0.1rem solid ${({ theme }) => theme.colors.silver};
 `;
 
+const TabContainer = styled.div`
+  display: flex;
+`;
+
 const Tab = styled.div<{ isActive: boolean }>`
-  width: 100%;
+  padding: 1rem 0;
+  width: max-content;
+  margin-left: 2.4rem;
   height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
   cursor: pointer;
-  border-bottom: ${({ isActive }) =>
-    isActive ? "0.1rem solid #1f8ecd" : "0.1rem solid #e6e6e6"};
+  font-size: ${({ theme }) => theme.fontSizes.sm};
+  color: ${({ theme }) => theme.colors.darkGray};
+  border-bottom: 0.1rem solid transparent;
+  :first-child {
+    margin-left: 1.6rem;
+  }
+
+  ${({ isActive, theme }) =>
+    isActive &&
+    css`
+      border-bottom: 0.1rem solid ${theme.colors.skyblue};
+      font-weight: bold;
+      color: ${theme.colors.skyblue};
+    `}
+
+  :hover {
+    color: ${({ theme }) => theme.colors.white};
+    border-bottom: 0.1rem solid ${({ theme }) => theme.colors.skyblue};
+    font-weight: bold;
+    color: ${({ theme }) => theme.colors.skyblue};
+  }
 `;
--- src/pages/summoners/Matchs/index.tsx
+++ src/pages/summoners/Matchs/index.tsx
@@ -1,7 +1,29 @@
+import { useMatchesQuery } from "@app/hooks/queries/useMatchesData";
 import React from "react";
+import { useParams } from "react-router-dom";
+import styled from "styled-components";
+import TabComponent from "./Tab";
+import { gameType } from "./types";
 
 const Matchs = () => {
-  return <div>Matchs</div>;
+  const params = useParams();
+
+  const { data: matchesData } = useMatchesQuery(params.summonerName!);
+
+  const [activeTab, setActiveTab] = React.useState<gameType>("all");
+
+  console.log(matchesData);
+
+  return (
+    <Container>
+      <TabComponent activeTab={activeTab} setActiveTab={setActiveTab} />
+    </Container>
+  );
 };
 
 export default Matchs;
+
+const Container = styled.div`
+  display: flex;
+  width: 100%;
+`;
